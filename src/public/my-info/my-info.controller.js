4cf9f2bec4790aa5e50bcc9e3504a4248eba814f
snTarball <30638696+snTarball@users.noreply.github.com>
2017-08-27 14:17:53 -0600

    initial

---
+++ src/public/my-info/my-info.controller.js
@@ -0,0 +1,14 @@
+
+(function () {
+"use strict";
+
+angular.module('public')
+.controller('MyInfoController', MyInfoController);
+
+MyInfoController.$inject = ['userInfo'];
+function MyInfoController(userInfo) {
+  var $myInfoCtrl = this;
+  $myInfoCtrl.user = userInfo;
+}
+
+})();
